862ebaf2fb61c8958bb1d454414de6c9bcaa837f
the-riquelme <riquelmedamiaosilva@gmail.com>
2023-12-29 19:59:27 -0300

    :sparkles: feat: implements initial project structure

--- src/assets/js/main.js
+++ src/assets/js/main.js
@@ -1,17 +1,16 @@
 document.getElementById('inject-graph').addEventListener('click', function () {
-  // Cria um elemento input de arquivo
   var inputFile = document.createElement('input');
   inputFile.type = 'file';
 
-  // Adiciona um evento para detectar quando um arquivo é selecionado
   inputFile.addEventListener('change', function () {
-    // Aqui você pode acessar o arquivo selecionado
     var selectedFile = inputFile.files[0];
-    console.log('Arquivo selecionado:', selectedFile);
-
-    // Adicione aqui o código para lidar com o arquivo selecionado, se necessário
+    enviarParaTeste(selectedFile);
   });
 
-  // Simula um clique no input de arquivo para abrir o diálogo de seleção de arquivo
   inputFile.click();
 });
+
+// Função para enviar o arquivo para teste
+function enviarParaTeste(arquivo) {
+  console.log('Enviando arquivo para teste:', arquivo);
+}
